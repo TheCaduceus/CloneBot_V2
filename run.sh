fcf5c91e84bb369c3873d940229976083362967b
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-24 14:50:51 +0530

    Grab Config.ini + Download Gclone + Port Error Fix!

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-CONFIG_FILE_URL = $CONFIG_FILE_URL
 rm telegram_gcloner/config.ini
 curl https://mirror-bot-resources:%40I2e32rffivdfb934or21e3qc@mirror-bot-configurations.thecaduceus.workers.dev/2:/gclone >> telegram_gcloner/gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
