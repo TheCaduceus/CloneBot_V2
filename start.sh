1908304b208948f07bb8558d8476efb1b026370e
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-24 14:00:15 +0530

    Grab Config.ini file + Port Error fix!

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ CONFIG_FILE_URL = $CONFIG_FILE_URL
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
-http-server -p $PORT &
+http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
