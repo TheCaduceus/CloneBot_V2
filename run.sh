0dcc0a0a96c5de95239211be6716755ce441d5ca
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-24 21:22:10 +0530

    Sync with Main

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
+curl https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/gclone >> telegram_gcloner/gclone
 npm install http-server -g
 http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
