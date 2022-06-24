47b51805ae258acdc4ac6f32bed6f93a1bcae958
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-24 19:09:44 +0530

    Change start.sh to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
-# curl 
 npm install http-server -g
 http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
