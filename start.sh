332f8edd14f87dbadfec6095413690e06170813d
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-24 13:59:28 +0530

    Grab Config.ini file!

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
-echo "[General]
-path_to_gclone = ./gclone
-telegram_token = $telegram_token
-user_ids = $user_ids
-group_ids = $group_ids
-gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+CONFIG_FILE_URL = $CONFIG_FILE_URL
+rm telegram_gcloner/config.ini
+curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
