53c4128fad7bbb4840ca8d9d1cfcd37371669436
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 00:17:50 +0530

    Get Gclone + Scalingo Support
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 rm telegram_gcloner/config.ini
+curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/AMD%2064/gclone" >> gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
 http-server -p 8080 &
