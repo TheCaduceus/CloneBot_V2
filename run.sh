f06059d18c57ca648c9dd17abe6841d86f6cffeb
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 01:51:48 +0530

    Download + Gclone
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm telegram_gcloner/config.ini
-curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/ARM%20v7/gclone" >> gclone
+curl "https://github.com/tiararosebiezetta/CloneBot_Scalingo/raw/master/gclone" >> gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
