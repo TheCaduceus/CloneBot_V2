7fc0f35aa7af78b2b679e545fc232ebca2c10be2
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 00:55:15 +0530

    Scalingo Support
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 rm telegram_gcloner/config.ini
 curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/AMD%2064/gclone" >> gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
-npm install http-server -g
-http-server -p 8080 &
+python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
