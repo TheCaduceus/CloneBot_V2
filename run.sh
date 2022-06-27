d75ec0a7d0ccd0934356357b1f11bc95331ebb68
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 01:35:30 +0530

    Download Gclone + Run Flask
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm telegram_gcloner/config.ini
-curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/ARM%2064/gclone" >> gclone
+curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/ARM/gclone" >> gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
