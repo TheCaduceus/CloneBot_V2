f3146fcc64ab7e544a4af5c86e171c381f77ff67
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 12:14:40 +0530

    Download Gclone + Grab Config.ini
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 rm telegram_gcloner/config.ini
-chmod +x ./gclone
 curl "https://clonebot.tk/0:/v1.58.1%20(Mango)/Linux/AMD%2064/gclone" >> gclone
+chmod +x ./gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
