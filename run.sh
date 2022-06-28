9c25de9b9c096e93a7c3e938b59bcd1597fc522b
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-06-28 14:13:14 +0530

    Grab `Config.ini` + Port `8080`
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
 http-server -p 8080 &
-python3 telegram_gcloner/telegram_gcloner.py
+python3 telegram_gcloner/telegram_gcloner.py 
