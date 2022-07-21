e57c822005c1a1b94bbea8c9c6e7f7ee21af62af
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-07-21 14:05:38 +0530

    Boost Up!🍺
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 npm install http-server -g
-python3 alert.py
 http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
