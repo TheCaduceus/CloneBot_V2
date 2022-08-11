e7505a4e347e788e744c6ddb764f92bb2e235612
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-08-11 18:47:29 +0530

    Upgrade gclone to `v1.59.1`
    Signed-off-by: Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm telegram_gcloner/config.ini
-curl "https://clonebot.tk/0:/Scalingo%20[R1.59.0]/gclone" >> gclone
+curl "https://clonebot.tk/0:/Scalingo%20[R1.59.1]/gclone" >> gclone
 chmod +x ./gclone
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 python3 clever.py &
